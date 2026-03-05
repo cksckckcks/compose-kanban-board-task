52580089c5cdf696252dc0fe536b3a1224f22392
cksckckcks <chan679999@naver.com>
2026-03-05 12:28:28 +0900

    feat: 태그 칩 그룹 작성

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/card/TagChipGroup.kt
@@ -0,0 +1,41 @@
+package woowacourse.kanban.board.component.card
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TagChipGroup(
+    tags: List<String>,
+    modifier: Modifier = Modifier
+) {
+    FlowRow(
+        modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        tags.forEachIndexed { index, tag ->
+            if (index < 5) {
+                TagChip(tag = tag)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TagChipGroupPreview() {
+    val tags = listOf("dd", "dd", "dd", "ddd", "Fff", "dd")
+    Box(
+        modifier = Modifier
+            .width(100.dp),
+    ) {
+        TagChipGroup(tags = tags)
+    }
+}
