3b7a122012dd303a2614c22bfa6d740fa7d2a001
cksckckcks <chan679999@naver.com>
2026-03-05 10:52:26 +0900

    feat: 카드 타이틀 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/card/CardTitle.kt
@@ -0,0 +1,33 @@
+package woowacourse.kanban.board.component.card
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun CardTitle(title: String? = null) {
+    Text(
+        text = title ?: "LazyColumn 컴포넌트 구현",
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 24.sp,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardTitlePreview() {
+    CardTitle()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardTitleMaxPreview() {
+    CardTitle(title = "너무너무 긴 제목은 한 줄까지만 노출너무너무 긴 제목은 한 줄까지만 노출...너무너무 긴 제목은 한 줄까지만 노출...너무너무 긴 제목은 한 줄까지만 노출...너무너무 긴 제목은 한 줄까지만 노출...너무너무 긴 제목은 한 줄까지만 노출...너무너무 긴 제목은 한 줄까지만 노출...")
+}
