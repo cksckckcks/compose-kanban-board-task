09649137957962b537a6cea954814851bbe74090
cksckckcks <chan679999@naver.com>
2026-03-08 00:45:28 +0900

    test: TitleText 컴포넌트 테스크 코드 작성

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/card/TitleTextTest.kt
@@ -0,0 +1,20 @@
+package woowacourse.kanban.board.component.card
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class TitleTextTest {
+    @Test
+    fun `제목이 정상적으로 표시된다`() = runComposeUiTest {
+        val title = "안녕하세요 제목입니다."
+
+        setContent {
+            TitleText(title)
+        }
+
+        onNodeWithText(title).assertExists()
+    }
+}
