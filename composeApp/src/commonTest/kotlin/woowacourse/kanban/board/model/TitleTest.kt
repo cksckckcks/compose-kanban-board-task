be6e00fbfc9beea34eb5fc2ca558e1d356bbfed2
cksckckcks <chan679999@naver.com>
2026-03-07 17:22:48 +0900

    test: Title 테스트 코드 작성

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TitleTest.kt
@@ -0,0 +1,34 @@
+package woowacourse.kanban.board.model
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TitleTest {
+    @Test
+    fun `제목이 정상적으로 생성된다`() {
+        val titleText = "안녕하세요. 제목입니다."
+
+        val title = Title(titleText)
+
+        assertEquals(titleText, title.text)
+    }
+
+    @Test
+    fun `제목이 비어있다면 생성되지 않는다`() {
+        val titleText = ""
+
+        assertFailsWith<IllegalArgumentException> {
+            Title(titleText)
+        }
+    }
+
+    @Test
+    fun `제목에 공백만 있다면 생성되지 않는다`() {
+        val titleText = "    "
+
+        assertFailsWith<IllegalArgumentException> {
+            Title(titleText)
+        }
+    }
+}
