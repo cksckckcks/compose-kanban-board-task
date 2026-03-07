7fd3d17ec4fcf7cb1b0ba9c98dcb61b9d060fe2a
cksckckcks <chan679999@naver.com>
2026-03-08 00:57:05 +0900

    test: Profile 테스트 코드 작성

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/card/ProfileTest.kt
@@ -0,0 +1,20 @@
+package woowacourse.kanban.board.component.card
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class ProfileTest {
+    @Test
+    fun `크루 이름이 정상적으로 표시된다`() = runComposeUiTest {
+        val crewName = "볼트"
+
+        setContent {
+            Profile(crewName)
+        }
+
+        onNodeWithText(crewName).assertExists()
+    }
+}
