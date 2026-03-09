da5ae86ea27f7b9571a730e2a51f6b12f874ed62
cksckckcks <chan679999@naver.com>
2026-03-09 10:34:06 +0900

    refactor: 크루 정상 테스트 케이스 테스트 함수 이름 변경

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/CrewTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/CrewTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertFailsWith
 
 class CrewTest {
     @Test
-    fun `크루가 정상적으로 생성된다`() {
+    fun `크루 이름이 있다면 크루가 생성된다`() {
         val crewName = "볼트"
 
         val name = Crew(crewName)
